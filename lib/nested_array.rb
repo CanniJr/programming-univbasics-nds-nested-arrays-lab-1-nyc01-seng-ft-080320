a6e3814fbe6746cf0f024cccda6f41899614b20b
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-30 20:29:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -43,6 +43,10 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  nested_array = [
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE]
+    nested_array[row][column]
   
 end
 
